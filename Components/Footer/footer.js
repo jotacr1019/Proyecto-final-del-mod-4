4a2345d1acdd72b51101f96e00410c9a01630e04
José J. Fernández Monge <76756167+jotacr1019@users.noreply.github.com>
2023-01-13 20:11:02 -0600

    Corrigiendo rutas

--- Components/Footer/footer.js
+++ Components/Footer/footer.js
@@ -13,18 +13,18 @@ function addFooter(elem) {
             <img class="footer__logo-services" src="/Assets/hombre-blanco.png" alt="logo-services"> Servicios
         </a>
         <a class="footer__opciones-contacto" href="contact.html">
-            <img class="footer__logo-contact" src="./Assets/tel-blanco.png" alt="logo-contact"> Contacto
+            <img class="footer__logo-contact" src="/Assets/tel-blanco.png" alt="logo-contact"> Contacto
         </a>
     </div>
     <div class="footer__redes-logos">
         <a class="footer__redes-linkedin" href="https://www.linkedin.com/in/jos%C3%A9-joaqu%C3%ADn-fern%C3%A1ndez-monge-6181a814a/">
-            <img class="footer__logo-linkedin" src="./Assets/linkedin-2.png" alt="logo-linkedin">
+            <img class="footer__logo-linkedin" src="/Assets/linkedin-2.png" alt="logo-linkedin">
         </a>
         <a class="footer__redes-github" href="https://github.com/jotacr1019">
-            <img class="footer__logo-github" src="./Assets/github-2.png" alt="logo-github">
+            <img class="footer__logo-github" src="/Assets/github-2.png" alt="logo-github">
         </a>
         <a class="footer__redes-instagram" href="https://www.instagram.com/josejoaquinfernandezmonge/">
-            <img class="footer__logo-instagram" src="./Assets/instagram-4.png" alt="logo-instagram">
+            <img class="footer__logo-instagram" src="/Assets/instagram-4.png" alt="logo-instagram">
         </a>
     </div>
     <div class="footer__derechos">
